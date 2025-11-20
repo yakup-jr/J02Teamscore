cea25b693c487b3cb5f5e70ff5d1be1830ec3a88
Anastasiys Korobetskaya <akorobetskaya@teamscore.ru>
2025-11-20 12:19:19 +0400

    Test for valid password added

--- src/tests/ru/teamscore/akorobetskaya/passwordvalidator/PasswordValidatorTest.java
+++ src/tests/ru/teamscore/akorobetskaya/passwordvalidator/PasswordValidatorTest.java
@@ -1,7 +1,23 @@
 package ru.teamscore.akorobetskaya.passwordvalidator;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PasswordValidatorTest {
 
+    @Test
+    @DisplayName("Valid password is accepted")
+    public void validPassword() {
+        // Arrange
+        String password = "Test6789";
+        String username = "TestUser";
+
+        // Act
+        boolean validationResult = PasswordValidator.isValidPassword(password, username);
+
+        // Assert
+        assertEquals(true, validationResult);
+    }
 }
